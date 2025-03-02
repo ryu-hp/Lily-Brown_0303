7fcca5528201d63673e95e256d3a520e3e84908b
Marin Miyazaki <web.namiya@gmail.com>
2025-03-02 11:37:52 +0900

    2025/03/02 11:40 SP実装途中

--- js/script.js
+++ js/script.js
@@ -1,8 +1,45 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  fadeAnimation();
+  replaceAnimation();
+
+  // swiper
+  const fastSwiper = new Swiper('.js-imgSliderFast', {
+    loop: true,
+    longSwipesRatio: true,
+    loopedSlides: 8,
+    speed: 4000,
+    slidesPerView: "auto",
+    loopAdditionalSlides: 8,
+    centeredSlides: true,
+    allowTouchMove: true,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    }
+  });
+
+  const slowSwiper = new Swiper('.js-imgSliderSlow', {
+    loop: true,
+    longSwipesRatio: true,
+    loopedSlides: 7,
+    speed: 5000,
+    slidesPerView: "auto",
+    loopAdditionalSlides: 7,
+    centeredSlides: true,
+    allowTouchMove: true,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    }
+  });
+});
+
 const ADDTIONCLASS = 'visible';
 const ISSP = window.matchMedia('(max-width: 767px)').matches;
 
 const fadeAnimation = () => {
-  detectionAnimation('.js-fadeInMv, .js-fadeImage, .js-rotateText');
+  detectionAnimation('.js-fadeInMv, .js-fadeIn, .js-fadeImage, .js-rotateText');
 }
 
 // 検知アニメーション
@@ -27,36 +64,21 @@ const detectionAnimation = (triggerElement) => {
   };
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  fadeAnimation();
-});
+// 画像切り替え
+const replaceAnimation = () => {
+  document.querySelectorAll('.js-fadeInReplace').forEach((ele) => {
+    let images = ele.querySelectorAll('.js-replaceImg');
+    let currentIndex = 0; // 表示中の画像のインデックス
 
-const fastSwiper = new Swiper('.js-imgSliderFast', {
-  loop: true,
-  longSwipesRatio: true,
-  loopedSlides: 8,
-  speed: 4000,
-  slidesPerView: "auto",
-  loopAdditionalSlides: 8,
-  centeredSlides: true,
-  allowTouchMove: true,
-  autoplay: {
-    delay: 0,
-    disableOnInteraction: false,
-  }
-});
+    setInterval(() => {
+      // 現在の画像の `visible` クラスを削除
+      images[currentIndex].classList.remove('visible');
 
-const slowSwiper = new Swiper('.js-imgSliderSlow', {
-  loop: true,
-  longSwipesRatio: true,
-  loopedSlides: 7,
-  speed: 5000,
-  slidesPerView: "auto",
-  loopAdditionalSlides: 7,
-  centeredSlides: true,
-  allowTouchMove: true,
-  autoplay: {
-    delay: 0,
-    disableOnInteraction: false,
-  }
-});
+      // 次の画像のインデックスを計算
+      currentIndex = (currentIndex + 1) % images.length;
+
+      // 次の画像に `visible` クラスを追加
+      images[currentIndex].classList.add('visible');
+    }, 4000); // 3秒ごとに切り替え
+  });
+}
